b9e1cf5e6f86e25a02572d2aace34022e5112962
adityasabat34 <147155098+adityasabat34@users.noreply.github.com>
2024-02-08 11:53:09 +0530

    some backend setup

---
+++ backend/server.js
@@ -0,0 +1,12 @@
+import exp from 'constants';
+import express from 'express';
+
+const app = express();
+
+app.get('/', (req, res) => {
+  res.send('API is running');
+});
+
+app.listen(5000, () => {
+  console.log('Server running on port 5000');
+});
