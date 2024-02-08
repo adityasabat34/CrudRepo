5a8c84894eede3253472060df67d7d697c3f594b
adityasabat34 <147155098+adityasabat34@users.noreply.github.com>
2024-02-08 12:29:26 +0530

    Create database model for data done.

--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,7 @@ import exp from 'constants';
 import express from 'express';
 import dotenv from 'dotenv';
 import connectDB from './config/db.js';
+import colors from 'colors';
 
 dotenv.config();
 
@@ -15,5 +16,7 @@ app.get('/', (req, res) => {
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
-  console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`);
+  console.log(
+    `Server running in ${process.env.NODE_ENV} mode on port ${PORT}`.bgBlue
+  );
 });
