11c57f3ab6dc65c51489db5674eb726e2be630e0
adityasabat34 <147155098+adityasabat34@users.noreply.github.com>
2024-02-10 19:43:07 +0530

    craete error message and form container components done.

---
+++ frontend/src/components/FormContainer.js
@@ -0,0 +1,19 @@
+import { Flex } from '@chakra-ui/react';
+
+const FormContainer = ({ children, width = 'xl' }) => {
+  return (
+    <Flex
+      direction="column"
+      boxShadow="md"
+      rounded="md"
+      bgColor="white"
+      p="10"
+      width={width}
+      border="1px solid black"
+    >
+      {children}
+    </Flex>
+  );
+};
+
+export default FormContainer;
