7a507b8d72317c1695676f358ca610192d510e3f
adityasabat34 <147155098+adityasabat34@users.noreply.github.com>
2024-02-11 16:04:33 +0530

    changes made

--- backend/server.js
+++ backend/server.js
@@ -4,6 +4,7 @@ import connectDB from './config/db.js';
 import colors from 'colors';
 import userRoutes from './routes/userRoutes.js';
 import { notFound, errorHandler } from './errors/errorHandler.js';
+import path from 'path';
 
 dotenv.config();
 
@@ -12,11 +13,9 @@ connectDB();
 const app = express();
 app.use(express.json());
 
-app.get('/', (req, res) => {
-  res.send('API is running');
-});
-
 app.use('/api/users', userRoutes);
+
+const __dirname = path.resolve();
 
 if (process.env.NODE_ENV === 'production') {
   app.use(express.static(path.join(__dirname, '/frontend/build')));
